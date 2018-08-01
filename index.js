32385cf2f516541373fa0e49819afc647b0141dd
fiolam <fiola.mulliqi@wework.com>
2018-08-01 22:21:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-document.addEventListener("DOMContentloaded", (e) => {
+//document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
+
   // your code here
-}
+
+  //debugger;
+//}
+
+
+$(document).ready(function(){
+  debugger;
+  $.getJSON("Movies.json"), function (response){
+  debugger;
+  }
